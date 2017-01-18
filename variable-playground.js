767aa42a4cf537698812901da1234fe76b9452cd
Sangjun Park <sjpark960520@gmail.com>
2017-01-18 23:06:35 +0900

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,29 @@
+//var person = {
+//    name: 'Andrew',
+//    age: 21
+//};
+//
+//function updatePerson (obj) {
+////    obj = {
+////        name: 'Andrew',
+////        age: 24
+////    };
+//    obj.age = 24;   
+//}
+//
+//updatePerson(person);
+//console.log(person);
+
+// Array Example
+var grades = [15, 88];
+
+function addGrades (gradesArr) {
+//    gradesArr.push(55);
+//    debugger; 
+//    
+    return gradesArr = [12, 33, 99];
+}
+
+
+grades = addGrades(grades);
+console.log(grades);
